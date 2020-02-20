8c12cc9939b8284beca9083358998ad681c7ce87
maxlab <maxlab6@hotmail.com>
2020-02-20 16:36:23 -0500

    Canvas sur char avec mini map
    mini map pas encore tout a fais fonctionnel

---
+++ New Unity Project/Assets/Voiture Prefabs/Script/CameraMiniMap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMiniMap : MonoBehaviour
+{
+    
+    public Transform trans;
+    
+    public Vector3 offset;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = trans.position  - offset; 
+    }
+}
