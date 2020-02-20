5607c194d70ea28d7fd37a9e1fbe328d5cdece2e
egadb <egadboisroy@gmail.com>
2020-02-20 16:21:37 -0500

    Menu1.01

---
+++ New Unity Project/Assets/MusicManager.cs
@@ -0,0 +1,70 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance;
+    private AudioSource audioSource;
+    private bool SongLoaded;
+    void Awake()
+    {
+
+        MakeSingleton();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+
+        if (!PlayerPrefs.HasKey("Game Initialized"))
+        {
+            MusicState.SetMusicState(1);
+
+            PlayerPrefs.SetInt(" Game Initialized", 123);
+        }
+
+    }
+
+
+
+
+    void MakeSingleton()
+    {
+
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+    public void PlayMusic(bool play)
+    {
+
+
+        if (play)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            if (audioSource.isPlaying)
+            {
+            }
+            audioSource.Stop();
+        }
+
+
+    }
+}
+*/
---
+++ New Unity Project/Assets/optionsPanelScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class optionsPanelScript : MonoBehaviour
+{
+    public AudioMixer mixer1;
+
+    float sliderValue = GameObject.Find("MusicSlider").GetComponent<Slider>().value;
+    public void SetMusicVolume(float sliderValue)
+    {
+
+        mixer1.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+
+    }
+}
