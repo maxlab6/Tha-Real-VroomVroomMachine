4434bb251325afa51f0a5a9b71f5b5abddc27050
ElTogo <felix.doyon@hotmail.com>
2020-03-09 21:19:10 -0400

    Menu Choix De Course Qui Marche
    les boutons des flèches seulement fonctionne

---
+++ New Unity Project/Assets/ButtonMenunChoixPiste.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonMenunChoixPiste : MonoBehaviour
+{
+    public Image imageC;
+    public Image imageG;
+    public Image imageD;
+
+    public Sprite spriteJ1;
+    public Sprite spriteJ2;
+    public Sprite spriteF1;
+
+    public int compteur = 0;
+    public static int valMax = 2;
+
+    public void buttonG()
+    {
+        if (compteur == 0)
+        {
+            compteur = valMax;
+        }
+        else
+        {
+            compteur -= 1;
+        }
+
+        if (compteur == 0)
+        {
+            imageC.overrideSprite = spriteF1;
+            imageG.overrideSprite = spriteJ2;
+            imageD.overrideSprite = spriteJ1;
+        }
+        if (compteur == 1)
+        {
+            imageC.overrideSprite = spriteJ2;
+            imageG.overrideSprite = spriteJ1;
+            imageD.overrideSprite = spriteF1;
+        }
+        if (compteur == 2)
+        {
+            imageC.overrideSprite = spriteJ1;
+            imageG.overrideSprite = spriteF1;
+            imageD.overrideSprite = spriteJ2;
+        }
+
+    }
+
+    public void buttonD()
+    {
+        if (compteur == valMax)
+        {
+            compteur = 0;
+        }
+        else
+        {
+            compteur += 1;
+        }
+
+        if (compteur == 0)
+        {
+            imageC.overrideSprite = spriteF1;
+            imageG.overrideSprite = spriteJ2;
+            imageD.overrideSprite = spriteJ1;
+        }
+        if (compteur == 1)
+        {
+            imageC.overrideSprite = spriteJ2;
+            imageG.overrideSprite = spriteJ1;
+            imageD.overrideSprite = spriteF1;
+        }
+        if (compteur == 2)
+        {
+            imageC.overrideSprite = spriteJ1;
+            imageG.overrideSprite = spriteF1;
+            imageD.overrideSprite = spriteJ2;
+        }
+
+    }
+
+}
