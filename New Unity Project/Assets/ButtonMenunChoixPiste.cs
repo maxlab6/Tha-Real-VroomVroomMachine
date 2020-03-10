a552805bfadd5e1d25a459a81e872a59467a8ca6
ElTogo <felix.doyon@hotmail.com>
2020-03-10 18:45:57 -0400

    mmmmmmm(*émoji qui doute*)
    juste une save pour pas perdre mon stock

--- New Unity Project/Assets/ButtonMenunChoixPiste.cs
+++ New Unity Project/Assets/ButtonMenunChoixPiste.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonMenunChoixPiste : MonoBehaviour
@@ -15,95 +17,94 @@ public class ButtonMenunChoixPiste : MonoBehaviour
     public Sprite spriteE1;
     public Sprite spriteE2;
 
-    public int compteur = 0;
     public static int valMax = 4;
-
-    public void buttonD()
-    {
-        if (compteur == 0)
-        {
-            compteur = valMax;
-        }
-        else
-        {
-            compteur -= 1;
-        }
-
-        if (compteur == 0)
-        {
-            imageG.overrideSprite = spriteJ2;
-            imageC.overrideSprite = spriteF1;
-            imageD.overrideSprite = spriteJ1;
-        }
-        if (compteur == 1)
-        {
-            imageG.overrideSprite = spriteE1;
-            imageC.overrideSprite = spriteJ2;
-            imageD.overrideSprite = spriteF1;
-        }
-        if (compteur == 2)
-        {
-            imageG.overrideSprite = spriteE2;
-            imageC.overrideSprite = spriteE1;
-            imageD.overrideSprite = spriteJ2;
-        }
-        if (compteur == 3)
-        {
-            imageG.overrideSprite = spriteJ1;
-            imageC.overrideSprite = spriteE2;
-            imageD.overrideSprite = spriteE1;
-        }
-        if (compteur == 4)
-        {
-            imageG.overrideSprite = spriteF1;
-            imageC.overrideSprite = spriteJ1;
-            imageD.overrideSprite = spriteE2;
-        }
-
-    }
 
     public void buttonG()
     {
-        if (compteur == valMax)
+        if (getCompteur() == 0)
         {
-            compteur = 0;
+            setCompteur(valMax);
         }
         else
         {
-            compteur += 1;
+            setCompteur(getCompteur()-1);
         }
 
-        if (compteur == 0)
+        print(getCompteur().ToString());
+
+        setImage(getCompteur());
+
+    }
+
+    public void buttonD()
+    {
+        if (getCompteur() == valMax)
+        {
+            setCompteur(0);
+        }
+        else
+        {
+            setCompteur(getCompteur()+1);
+        }
+
+        print(getCompteur().ToString());
+
+        setImage(getCompteur());
+
+    }
+
+    public void setCompteur(int c)
+    {
+        GameObject.Find("GameObjectCompteur").GetComponent<scriptCompteur>().setCompteur(c);
+    }
+    public int getCompteur()
+    {
+        return GameObject.Find("GameObjectCompteur").GetComponent<scriptCompteur>().getCompteur();
+    }
+
+    public void setImage(int c)
+    {
+        if (c == 0)
         {
             imageG.overrideSprite = spriteJ2;
             imageC.overrideSprite = spriteF1;
             imageD.overrideSprite = spriteJ1;
         }
-        if (compteur == 1)
+        if (c == 1)
         {
             imageG.overrideSprite = spriteE1;
             imageC.overrideSprite = spriteJ2;
             imageD.overrideSprite = spriteF1;
         }
-        if (compteur == 2)
+        if (c == 2)
         {
             imageG.overrideSprite = spriteE2;
             imageC.overrideSprite = spriteE1;
             imageD.overrideSprite = spriteJ2;
         }
-        if (compteur == 3)
+        if (c == 3)
         {
             imageG.overrideSprite = spriteJ1;
             imageC.overrideSprite = spriteE2;
             imageD.overrideSprite = spriteE1;
         }
-        if (compteur == 4)
+        if (c == 4)
         {
             imageG.overrideSprite = spriteF1;
             imageC.overrideSprite = spriteJ1;
             imageD.overrideSprite = spriteE2;
         }
 
+        
     }
 
+    public void buttonChoisir()
+    {
+        if (getCompteur() == 0)
+        {
+            print("hold on");
+            SceneManager.LoadScene("PisteFelix", LoadSceneMode.Additive);
+            print("ca marche pas");
+        }
+    }
 }
