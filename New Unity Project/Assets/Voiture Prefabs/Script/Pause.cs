c60fc451d327616cabdd581c4ceaa47d90530e8c
Jonathan <jonathanpilon21@gmail.com>
2020-03-13 00:27:05 -0400

    Menu pause (escape pour pause)

---
+++ New Unity Project/Assets/Voiture Prefabs/Script/Pause.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+	public Button retour;
+	public GameObject paneau;
+	public static bool GameIsPause = false;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+		{
+			Stop();
+		}
+
+		retour.onClick.AddListener(Continuer);
+
+		
+    }
+
+	private void Continuer()
+	{
+		paneau.SetActive(false);
+		Time.timeScale = 1f;
+	}
+
+	private void Stop ()
+	{
+		paneau.SetActive(true);
+		Time.timeScale = 0f;
+	}
+}
