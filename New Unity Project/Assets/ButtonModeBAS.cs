5d705472f6435a6ea29a039e7f8d022451f2ec10
ElTogo <felix.doyon@hotmail.com>
2020-04-05 18:23:38 -0400

    Boutons des menus
    J'ai setté le bouton pour le bac a sable (qui fonctionne) et ceux du menu pause (mais qui ne fonctionnent pas :( pis je sais pas pourquoi)

---
+++ New Unity Project/Assets/ButtonModeBAS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonModeBAS : MonoBehaviour
+{
+    public void buttonBAC()
+    {
+        SceneManager.LoadScene("Sandbox", LoadSceneMode.Single);
+    }
+}
---
+++ New Unity Project/Assets/ButtonQuitter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonQuitter : MonoBehaviour
+{
+    public void buttonMenuP()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+
+    public void buttonQuitter()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit ();
+    #endif
+    }
+}
