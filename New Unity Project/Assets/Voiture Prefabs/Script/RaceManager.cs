9aa57ab9c8b1481cdb645cba9d8945910f5eee32
ElTogo <felix.doyon@hotmail.com>
2020-05-14 19:09:52 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 56fc977b211c2eeb83f12cd35f01b2f5fca4a5d5.

--- New Unity Project/Assets/Voiture Prefabs/Script/RaceManager.cs
+++ New Unity Project/Assets/Voiture Prefabs/Script/RaceManager.cs
@@ -1,32 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class RaceManager : MonoBehaviour
 {
-
-    public Transform[] allCars;
-    public float[] carOrder;
-
+    public CarController[] allCars;
+    public CarController[] carOrder;
+    
     public void Start()
     {
         // set up the car objects
-        carOrder = new float[allCars.Length];
+        carOrder = new CarController[allCars.Length];
         InvokeRepeating("ManualUpdate", 0.5f, 0.5f);
     }
-    /*
+    
     // this gets called every frame
     public void ManualUpdate()
     {
-        foreach (Transform car in allCars)
+        foreach (CarController car in allCars)
         {
             carOrder[car.GetCarPos(allCars) - 1] = car;
         }
         Debug.Log(carOrder[0]);
     }
-    */
 }
-
-
-
