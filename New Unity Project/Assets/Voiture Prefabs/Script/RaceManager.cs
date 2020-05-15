383544c62e767b82f76038f2777bf4925f02c06a
Maxlab <maxlab6@hotmail.com>
2020-05-14 20:27:03 -0400

    Les tours fonctionnes

--- New Unity Project/Assets/Voiture Prefabs/Script/RaceManager.cs
+++ New Unity Project/Assets/Voiture Prefabs/Script/RaceManager.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class RaceManager : MonoBehaviour
 {
-    public CarController[] allCars;
-    public CarController[] carOrder;
+    public PositionVoiture[] allCars;
+    public PositionVoiture[] carOrder;
     
     public void Start()
     {
         // set up the car objects
-        carOrder = new CarController[allCars.Length];
+        carOrder = new PositionVoiture[allCars.Length];
         InvokeRepeating("ManualUpdate", 0.5f, 0.5f);
     }
     
     // this gets called every frame
     public void ManualUpdate()
     {
-        foreach (CarController car in allCars)
+        foreach (PositionVoiture car in allCars)
         {
             carOrder[car.GetCarPos(allCars) - 1] = car;
         }
