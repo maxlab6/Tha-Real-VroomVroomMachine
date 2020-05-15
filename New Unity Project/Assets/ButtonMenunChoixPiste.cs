c77b2d6762bf9234d59bd5dad0cd0cc19f45689e
Maxlab <maxlab6@hotmail.com>
2020-05-15 16:50:29 -0400

    Menu Course avec les 3 pistes

--- New Unity Project/Assets/ButtonMenunChoixPiste.cs
+++ New Unity Project/Assets/ButtonMenunChoixPiste.cs
@@ -11,14 +11,12 @@ public class ButtonMenunChoixPiste : MonoBehaviour
     public Image imageG;
     public Image imageD;
 
-    public Sprite spriteJ1;
-    public Sprite spriteJ2;
     public Sprite spriteF1;
     public Sprite spriteE1;
     public Sprite spriteE2;
 
     public static int compteur = 0;
-    public static int valMax = 4;
+    public static int valMax = 2;
 
     public void buttonD()
     {
@@ -70,32 +68,21 @@ public class ButtonMenunChoixPiste : MonoBehaviour
         switch (c)
         {
             case 0:
-            imageG.overrideSprite = spriteJ2;
-            imageC.overrideSprite = spriteF1;
-            imageD.overrideSprite = spriteJ1;
-                break;
-            case 1:
-            imageG.overrideSprite = spriteE1;
-            imageC.overrideSprite = spriteJ2;
-            imageD.overrideSprite = spriteF1;
-                break;
-            case 2:
             imageG.overrideSprite = spriteE2;
-            imageC.overrideSprite = spriteE1;
-            imageD.overrideSprite = spriteJ2;
-                break;
-            case 3:
-            imageG.overrideSprite = spriteJ1;
-            imageC.overrideSprite = spriteE2;
+            imageC.overrideSprite = spriteF1;
             imageD.overrideSprite = spriteE1;
                 break;
-            case 4:
+            case 1:
             imageG.overrideSprite = spriteF1;
-            imageC.overrideSprite = spriteJ1;
+            imageC.overrideSprite = spriteE1;
             imageD.overrideSprite = spriteE2;
                 break;
-            default:
+            case 2:
+            imageG.overrideSprite = spriteE1;
+            imageC.overrideSprite = spriteE2;
+            imageD.overrideSprite = spriteF1;
                 break;
+            
         }
 
         
@@ -111,17 +98,12 @@ public class ButtonMenunChoixPiste : MonoBehaviour
                 break;
 
             case 1:
-                SceneManager.LoadScene("PisteJoV2", LoadSceneMode.Single);
-                break;
-            case 2:
                 SceneManager.LoadScene("PisteÉlian1", LoadSceneMode.Single);
                 break;
-            case 3:
+            case 2:
                 SceneManager.LoadScene("PisteÉlian2", LoadSceneMode.Single);
                 break;
-            case 4:
-                SceneManager.LoadScene("MapJo", LoadSceneMode.Single);
-                break;
+            
         }
     }
 }
