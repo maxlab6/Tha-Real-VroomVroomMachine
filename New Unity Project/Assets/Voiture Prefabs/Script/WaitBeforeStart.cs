d683238883768925e4772db267bd6f5962bfa182
Maxlab <maxlab6@hotmail.com>
2020-05-17 18:40:47 -0400

    C celle la

--- New Unity Project/Assets/Voiture Prefabs/Script/WaitBeforeStart.cs
+++ New Unity Project/Assets/Voiture Prefabs/Script/WaitBeforeStart.cs
@@ -6,10 +6,19 @@ public class WaitBeforeStart : MonoBehaviour
 {
 
     public GameObject countDown;
+    public GameObject menuPause;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine("StartCountDown");
+    }
+
+    private void Update()
+    {
+        if (menuPause.active)
+        {
+            countDown.SetActive(false);
+        }
     }
 
     IEnumerator StartCountDown()
