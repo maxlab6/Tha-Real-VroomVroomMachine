c8404a436f6450716c59635c7fc41f60f4cd58ab
Maxlab <maxlab6@hotmail.com>
2020-05-17 03:38:35 -0400

    Supprimé partie nico, map inutile et ajouté 3,2,1 GO

--- New Unity Project/Assets/Voiture Prefabs/Script/WaitBeforeStart.cs
+++ New Unity Project/Assets/Voiture Prefabs/Script/WaitBeforeStart.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class WaitBeforeStart : MonoBehaviour
 {
+
+    public GameObject countDown;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("StartCountDown");
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator StartCountDown()
     {
-        
+        Time.timeScale = 0;
+        float pausedTime = Time.realtimeSinceStartup + 4f;
+        while (Time.realtimeSinceStartup < pausedTime)
+        {
+            yield return 0;
+        }
+
+       
+        countDown.gameObject.SetActive(false);
+        Time.timeScale = 1;
     }
 }
